883d876fc47dfe251c06b97a9d2ac5cf59f0da68
imSafdarAwan <imsafdarawan@gmail.com>
2022-06-19 20:49:40 +0500

    refactor and re-write the config for dashboard and nvim-tree

--- lua/plugins/configs/dashboard.lua
+++ lua/plugins/configs/dashboard.lua
@@ -1,9 +1,6 @@
 local g = vim.g
-g.dashboard_disable_at_vimenter = 0
-g.dashboard_disable_statusline = 1
-g.dashboard_default_executive = "telescope"
-
-g.dashboard_custom_header = {
+local db = require("dashboard")
+db.custom_header = {
 	"                                   ",
 	"                                   ",
 	"                                   ",
@@ -20,26 +17,19 @@ g.dashboard_custom_header = {
 	"       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
 	"                                   ",
 }
-
-g.dashboard_custom_section = {
-	a = {
-		description = { "Keep it Minimal" },
-		command = "",
-	},
-	b = {
-		description = { "Keep it Clean" },
-		command = "",
-	},
-	-- a = {
-	-- 	description = { "  Find File                 leader f f" },
-	-- 	command = "Telescope find_files",
-	-- },
-	-- b = { description = { "  Recents                   leader f o" }, command = "Telescope oldfiles" },
-	-- c = { description = { "  Find Word                 leader f g" }, command = "Telescope live_grep" },
-	-- d = { description = { "洛 New File                  leader f n" }, command = "DashboardNewFile" },
-	-- e = { description = { "  Bookmarks                 leader b m" }, command = "Telescope marks" },
-	-- f = { description = { "  Load Last Session         leader l  " }, command = "SessionLoad" },
+db.custom_center = {
+	-- { icon = "some icon", desc = "some description here" ,action = "Telescope find files" },
+	{ desc = "Kepp It Minimal" },
+	{ desc = "Keep It Simple" },
 }
+db.custom_footer = {
+	"@imSafdarAwan",
+}
+-- 
+-- 
+-- 
+-- 
+-- 
 
 g.dashboard_custom_footer = {
 	"@imSafdarAwan",
--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -1,124 +1,175 @@
--- local vim = vim
--- local g = vim.g
---
--- g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
--- g.nvim_tree_git_hl = 0
--- g.nvim_tree_group_empty = 1
--- g.nvim_tree_create_in_closed_folder = 0
--- g.nvim_tree_refresh_wait = 500
--- g.nvim_tree_root_folder_modifier = table.concat({ ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" })
--- g.nvim_tree_icons = {
--- 	default = "",
--- 	symlink = "",
--- 	git = {
--- 		deleted = "",
--- 		ignored = "◌",
--- 		renamed = "➜",
--- 		staged = "✓",
--- 		unmerged = "",
--- 		unstaged = "✗",
--- 		untracked = "★",
--- 	},
--- 	folder = {
--- 		-- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
--- 		-- arrow_open = "",
--- 		-- arrow_closed = "",
--- 		default = "",
--- 		empty = "", -- 
--- 		empty_open = "",
--- 		open = "",
--- 		symlink = "",
--- 		symlink_open = "",
--- 	},
--- }
--- -- g.nvim_tree_show_icons = {
--- -- 	git = 1,
--- -- 	folders = 1,
--- -- 	files = 1,
--- -- 	folder_arrows = 1,
--- -- }
--- require("nvim-tree").setup({
--- 	disable_netrw = false,
--- 	hijack_netrw = true,
--- 	hijack_directories = {
--- 		enable = true,
--- 		auto_open = true,
--- 	},
--- 	renderer = {
--- 		indent_markers = {
--- 			enable = false,
--- 			icons = {
--- 				corner = "└ ",
--- 				edge = "│ ",
--- 				none = "  ",
--- 			},
--- 		},
--- 		icons = {
--- 			webdev_colors = true,
--- 		},
--- 	},
--- 	auto_reload_on_write = true,
--- 	ignore_ft_on_setup = { "dashboard" },
--- 	ignore_buffer_on_setup = false,
--- 	-- auto_close = false,
--- 	open_on_tab = false,
--- 	sort_by = "name",
--- 	hijack_cursor = true,
--- 	update_cwd = true,
--- 	hijack_unnamed_buffer_when_opening = true,
--- 	-- git = true,
--- 	diagnostics = {
--- 		enable = true,
--- 		icons = {
--- 			hint = "",
--- 			info = "",
--- 			warning = "",
--- 			error = "",
--- 		},
--- 	},
--- 	update_focused_file = {
--- 		enable = true,
--- 		update_cwd = false,
--- 	},
--- 	filters = {
--- 		dotfiles = true,
--- 		custom = { "node_modules", "package.json", "package-lock.json", ".git" },
--- 	},
--- 	trash = {
--- 		cmd = "trash",
--- 		require_confirm = true,
--- 	},
--- 	view = {
--- 		width = 30,
--- 		hide_root_folder = false,
--- 		side = "right",
--- 		height = 30,
--- 		preserve_window_proportions = false,
--- 		number = false,
--- 		relativenumber = false,
--- 		signcolumn = "yes",
--- 		mappings = {
--- 			custom_only = false,
--- 			list = {},
--- 		},
--- 		-- auto_resize = true,
--- 	},
--- 	actions = {
--- 		change_dir = {
--- 			enable = true,
--- 			global = false,
--- 		},
--- 		open_file = {
--- 			quit_on_open = false,
--- 			resize_window = false,
--- 			window_picker = {
--- 				enable = true,
--- 				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
--- 				exclude = {
--- 					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
--- 					buftype = { "nofile", "terminal", "help" },
--- 				},
--- 			},
--- 		},
--- 	},
--- })
+local vim = vim
+local g = vim.g
+
+require("nvim-tree").setup({
+	auto_reload_on_write = true,
+	create_in_closed_folder = false,
+	disable_netrw = false,
+	hijack_cursor = true,
+	hijack_netrw = true,
+	hijack_unnamed_buffer_when_opening = true,
+	ignore_buffer_on_setup = false,
+	open_on_setup = false,
+	open_on_setup_file = false,
+	open_on_tab = false,
+	sort_by = "name",
+	update_cwd = true,
+	reload_on_bufenter = false,
+	respect_buf_cwd = false,
+	view = {
+		adaptive_size = false,
+		centralize_selection = false,
+		width = 30,
+		height = 30,
+		hide_root_folder = true,
+		side = "right",
+		preserve_window_proportions = false,
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+		mappings = {
+			custom_only = false,
+			list = {
+				-- user mappings go here
+			},
+		},
+	},
+	renderer = {
+		add_trailing = false,
+		group_empty = false,
+		highlight_git = false,
+		full_name = false,
+		highlight_opened_files = "none",
+		root_folder_modifier = ":~",
+		indent_markers = {
+			enable = false,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				item = "│ ",
+				none = "  ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+			git_placement = "before",
+			padding = " ",
+			symlink_arrow = " ➛ ",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					arrow_closed = "",
+					arrow_open = "",
+					default = "",
+					open = "",
+                    empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+	},
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = false,
+		ignore_list = {},
+	},
+	ignore_ft_on_setup = {"dashboard"},
+	system_open = {
+		cmd = "",
+		args = {},
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	filters = {
+		dotfiles = true,
+		custom = { "node_modules", "package.json", "package-lock.json", ".git" },
+		exclude = {},
+	},
+	filesystem_watchers = {
+		enable = false,
+		interval = 100,
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 400,
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		expand_all = {
+			max_folder_discovery = 300,
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = true,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+		remove_file = {
+			close_window = true,
+		},
+	},
+	trash = {
+		cmd = "gio trash",
+		require_confirm = true,
+	},
+	live_filter = {
+		prefix = "[FILTER]: ",
+		always_show_folders = true,
+	},
+	log = {
+		enable = false,
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
+			watcher = false,
+		},
+	},
+})
