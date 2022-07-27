ad49d87a9bcf1bed8ea6c95c1a39aa5839e3cbc2
TheSafdarAwan <thesafdarawan@gmail.com>
2022-07-28 02:03:56 +0500

    remove the diff-preview plugin config

--- lua/core/plugins_mappings/diff_view_map.lua
+++
@@ -1,4 +0,0 @@
-local map = require("core.utils").map
--- Diff View
-map("n", "<leader>dv", ":DiffviewOpen<cr>", { noremap = true, silent = true })
-map("n", "<leader>dx", ":DiffviewClose<cr>", { noremap = true, silent = true })
