530d78b725c70d59d7cf55f2397bc2db313247f0
TheSafdarAwan <thesafdarawan@gmail.com>
2022-08-12 19:55:27 +0500

    change the suggestion selection color

--- lua/safdar/lush_theme/theme-sid/theme-sid.lua
+++ lua/safdar/lush_theme/theme-sid/theme-sid.lua
@@ -43,7 +43,7 @@ local theme = lush(function()
 		NormalFloat({ bg = colors.color_3 }), -- Normal text in floating windows.
 		NormalNC({ fg = colors.fg, bg = Config.transparent_background and "NONE" or colors.bg }), -- normal text in non-current windows
 		Pmenu({ fg = colors.color_25, bg = colors.color_2 }), -- Popup menu: normal item.
-		PmenuSel({ fg = colors.color_1, bg = colors.color_23 }), -- Popup menu: selected item.
+		PmenuSel({ fg = colors.color_1, bg = colors.color_17 }), -- Popup menu: selected item.
 		PmenuSbar({ bg = colors.color_8 }), -- Popup menu: scrollbar.
 		PmenuThumb({ bg = colors.color_21 }), -- Popup menu: Thumb of the scrollbar.
 		Question({ fg = colors.color_20 }), -- |hit-enter| prompt and yes/no questions
