c9198628e857c7f0781d93f275cd2c2c8bb69130
TheSafdarAwan <thesafdarawan@gmail.com>
2022-10-05 16:30:43 +0500

    remove theme

--- lua/safdar/plugins/packer.lua
+++ lua/safdar/plugins/packer.lua
@@ -36,32 +36,6 @@ packer.startup({
 
         --> color scheme
         use({ "rktjmp/lush.nvim" })
-        use({ "folke/tokyonight.nvim" })
-        use({
-            "ellisonleao/gruvbox.nvim",
-            config = function()
-                require("gruvbox").setup({
-                    undercurl = true,
-                    underline = true,
-                    bold = true,
-                    italic = true,
-                    strikethrough = true,
-                    invert_selection = false,
-                    invert_signs = false,
-                    invert_tabline = false,
-                    invert_intend_guides = false,
-                    inverse = true, -- invert background for search, diffs, statuslines and errors
-                    contrast = "", -- can be "hard", "soft" or empty string
-                    overrides = {},
-                    dim_inactive = false,
-                    transparent_mode = false,
-                })
-            end
-        })
-        use({
-            "catppuccin/nvim",
-            as = "catppuccin",
-        })
 
         use({
             "folke/todo-comments.nvim",
