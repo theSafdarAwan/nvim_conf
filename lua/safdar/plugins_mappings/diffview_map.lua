bcf3f68c24eb469df1bc9d87e0bbed0ac2d8de85
TheSafdarAwan <thesafdarawan@gmail.com>
2022-11-06 17:45:56 +0500

    Plugin Mod: move the diffview internal maps to diffview_map file

--- lua/safdar/plugins_mappings/diffview_map.lua
+++ lua/safdar/plugins_mappings/diffview_map.lua
@@ -1,4 +1,5 @@
 local M = {}
+local actions = require("diffview.actions")
 
 M.maps = function()
     local utils = require("safdar.core.utils")
@@ -9,5 +10,81 @@ M.maps = function()
     map("n", "<C-g>D", "<cmd>DiffviewFileHistory<CR>", opts)
     map("n", "<C-g>x", "<cmd>DiffviewClose<CR>", opts)
 end
+
+M.keymaps = {}
+
+M.keymaps.view = {
+    ["J"] = actions.select_next_entry,
+    ["K"] = actions.select_prev_entry,
+    ["<C-w>"] = actions.goto_file_split,
+    ["gf"] = actions.goto_file_tab,
+    ["E"] = actions.focus_files,
+    ["T"] = actions.toggle_files,
+    ["gC"] = actions.cycle_layout,
+    ["[x"] = actions.prev_conflict,
+    ["]x"] = actions.next_conflict,
+    ["<leader>co"] = actions.conflict_choose("ours"),
+    ["<leader>ct"] = actions.conflict_choose("theirs"),
+    ["<leader>cb"] = actions.conflict_choose("base"),
+    ["<leader>ca"] = actions.conflict_choose("all"),
+    ["<leader>cn"] = actions.conflict_choose("none"),
+}
+M.keymaps.diff3 = {
+    { { "n", "x" }, "2do", actions.diffget("ours") },
+    { { "n", "x" }, "3do", actions.diffget("theirs") },
+}
+M.keymaps.diff4 = {
+    { { "n", "x" }, "1do", actions.diffget("base") },
+    { { "n", "x" }, "2do", actions.diffget("ours") },
+    { { "n", "x" }, "3do", actions.diffget("theirs") },
+}
+M.keymaps.file_panel = {
+    ["j"] = actions.next_entry,
+    ["k"] = actions.prev_entry,
+    ["<tab>"] = actions.select_entry,
+    ["-"] = actions.toggle_stage_entry,
+    ["<C-s>"] = actions.stage_all,
+    ["U"] = actions.unstage_all,
+    ["X"] = actions.restore_entry,
+    ["L"] = actions.open_commit_log,
+    ["<c-b>"] = actions.scroll_view(-0.25),
+    ["<c-f>"] = actions.scroll_view(0.25),
+    ["J"] = actions.select_next_entry,
+    ["K"] = actions.select_prev_entry,
+    ["<C-w>"] = actions.goto_file_split,
+    ["gf"] = actions.goto_file_tab,
+    ["i"] = actions.listing_style,
+    ["f"] = actions.toggle_flatten_dirs,
+    ["R"] = actions.refresh_files,
+    ["E"] = actions.focus_files,
+    ["T"] = actions.toggle_files,
+    ["gC"] = actions.cycle_layout,
+    ["[x"] = actions.prev_conflict,
+    ["]x"] = actions.next_conflict,
+}
+M.keymaps.file_history_panel = {
+    ["g!"] = actions.options,
+    ["<C-A-d>"] = actions.open_in_diffview,
+    ["y"] = actions.copy_hash,
+    ["L"] = actions.open_commit_log,
+    ["zr"] = actions.open_all_folds,
+    ["zm"] = actions.close_all_folds,
+    ["j"] = actions.next_entry,
+    ["k"] = actions.prev_entry,
+    ["<tab>"] = actions.select_entry,
+    ["<c-b>"] = actions.scroll_view(-0.25),
+    ["<c-f>"] = actions.scroll_view(0.25),
+    ["J"] = actions.select_next_entry,
+    ["K"] = actions.select_prev_entry,
+    ["<C-w>gf"] = actions.goto_file_split,
+    ["gf"] = actions.goto_file_tab,
+    ["E"] = actions.focus_files,
+    ["T"] = actions.toggle_files,
+    ["gC"] = actions.cycle_layout,
+}
+M.keymaps.option_panel = {
+    ["<tab>"] = actions.select_entry,
+    ["q"] = actions.close,
+}
 
 return M
