d2392b07beac02f9c75b8f7ba7818ef480e1dadc
TheSafdarAwan <thesafdarawan@gmail.com>
2022-11-12 13:59:23 +0500

    Plugin Conf: change the cmp luasnip jumping behavior to local rather then global

--- lua/safdar/lsp/cmp.lua
+++ lua/safdar/lsp/cmp.lua
@@ -62,7 +62,8 @@ cmp.setup({
         ["<Tab>"] = function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+            -- elseif luasnip.expand_or_jumpable() then
+            elseif luasnip.expand_or_locally_jumpable() then
                 luasnip.expand_or_jump()
             else
                 fallback()
