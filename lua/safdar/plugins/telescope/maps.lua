ab95031f674a1a34cdc2f8b123bc4c30bce1b95e
TheSafdarAwan <thesafdarawan@gmail.com>
2022-11-18 19:41:13 +0500

    refactoring

--- lua/safdar/plugins/telescope/maps.lua
+++ lua/safdar/plugins/telescope/maps.lua
@@ -55,59 +55,41 @@ map(
     ":Telescope builtin<cr>",
     { noremap = true, silent = true }
 )
-map(
-    "n",
-    "<leader>en",
-    ":lua require('safdar.plugins.telescope').search_nvim_dotfiles()<cr>",
-    { noremap = true, silent = true }
-)
-map(
-    "n",
-    "<leader>gtd",
-    ":lua require('safdar.plugins.telescope').gtd_neorg_files()<cr>",
-    { noremap = true, silent = true }
-)
 
-map(
-    "n",
-    "<leader>f;",
-    ":lua require('safdar.plugins.telescope').ivy_find_files_with_preview()<cr>",
-    { noremap = true, silent = true }
-)
-map(
-    "n",
-    "<leader>fb",
-    ":lua require('safdar.plugins.telescope').center_list_buffers_find()<cr>",
-    { noremap = true, silent = true }
-)
-map(
-    "n",
-    "<leader>tc",
-    ":lua require('safdar.plugins.telescope').get_cursor_change_colorscheme()<cr>",
-    { noremap = true, silent = true }
-)
-map(
-    "n",
-    "<leader>ff",
+map("n", "<leader>en", function()
+    require("safdar.plugins.telescope").search_nvim_dotfiles()
+end, { noremap = true, silent = true })
+
+map("n", "<leader>gtd", function()
+    require("safdar.plugins.telescope").gtd_neorg_files()
+end, { noremap = true, silent = true })
+
+map("n", "<leader>f;", function()
+    require("safdar.plugins.telescope").ivy_find_files_with_preview()
+end, { noremap = true, silent = true })
+
+map("n", "<leader>fb", function()
+    require("safdar.plugins.telescope").center_list_buffers_find()
+end, { noremap = true, silent = true })
+
+map("n", "<leader>tc", function()
+    require("safdar.plugins.telescope").get_cursor_change_colorscheme()
+end, { noremap = true, silent = true })
+
+map("n", "<leader>ff", function()
     -- ":Telescope find_files theme=get_cursor previewer=false find_command=rg,--hidden,--files<CR>",
-    ":lua require('safdar.plugins.telescope').center_list_find_files()<cr>",
-    { noremap = true, silent = true }
-)
-map(
-    "n",
-    "<C-_>",
-    ":lua require('safdar.plugins.telescope').current_buffer_fuzzy_find()<cr>",
-    { noremap = true, silent = true }
-)
-map(
-    "n",
-    "<leader>ds",
-    ":lua require('safdar.plugins.center_list_document_symbols_find()<cr>",
-    { noremap = true, silent = true }
-)
--- <--
+    require("safdar.plugins.telescope").center_list_find_files()
+end, { noremap = true, silent = true })
 
--- --> Telescope core mappings
+map("n", "<C-_>", function()
+    require("safdar.plugins.telescope").current_buffer_fuzzy_find()
+end, { noremap = true, silent = true })
+
+map("n", "<leader>ds", function()
+    require("safdar.plugins.telescope").center_list_document_symbols_find()
+end, { noremap = true, silent = true })
+
+-- Telescope core mappings
 local M = {}
 M.mappings = function()
     return {
@@ -121,4 +103,4 @@ M.mappings = function()
     }
 end
 return M
--- <--
+--
