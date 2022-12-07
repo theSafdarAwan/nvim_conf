9e50cf2ad027ff5d6dda5dfba6bca1006ea4274e
TheSafdarAwan <thesafdarawan@gmail.com>
2022-12-07 08:54:50 +0500

    Change the timer_start to vim.defer_fn

--- after/plugin/personal-plugins/lazy-load-packer.lua
+++ after/plugin/personal-plugins/lazy-load-packer.lua
@@ -3,7 +3,6 @@ local utils = require("safdar.core.utils")
 local vim = utils.vim
 local api = vim.api
 local bo = vim.bo
-local fn = vim.fn
 local packer_plugins = packer_plugins
 
 local packer = require("packer")
@@ -11,14 +10,14 @@ local packer = require("packer")
 local plugin_name
 local au_group = api.nvim_create_augroup("__packer_lazy_loading_plugins", { clear = true })
 
-local function timer(name, time)
-	fn.timer_start(time, function()
+local function defer_fn(name, time)
+	vim.defer_fn(function()
 		if packer_plugins[name] then
 			if not packer_plugins[name].loaded then
 				packer.loader(name)
 			end
 		end
-	end)
+	end, time)
 end
 
 api.nvim_create_autocmd({ "InsertEnter" }, {
@@ -35,7 +34,7 @@ api.nvim_create_autocmd({ "InsertEnter" }, {
 	group = au_group,
 	callback = function()
 		plugin_name = "cmp-tabnine"
-		timer(plugin_name, 3000)
+		defer_fn(plugin_name, 3000)
 	end,
 })
 
@@ -46,7 +45,7 @@ api.nvim_create_autocmd({ "InsertEnter" }, {
 		for _, f in pairs(ft) do
 			if bo.filetype == f then
 				plugin_name = "cmp-dictionary"
-				timer(plugin_name, 4000)
+				defer_fn(plugin_name, 4000)
 			end
 		end
 	end,
@@ -72,7 +71,7 @@ api.nvim_create_autocmd(
 		callback = function()
 			plugin_name = "ale"
 			if bo.filetype == "html" then
-				timer(plugin_name, 1000)
+				defer_fn(plugin_name, 1000)
 			end
 		end,
 	} --
@@ -82,7 +81,7 @@ api.nvim_create_autocmd({ "VimEnter" }, {
 	group = au_group,
 	callback = function()
 		plugin_name = "nvim-lspconfig"
-		timer(plugin_name, 2000)
+		defer_fn(plugin_name, 2000)
 	end,
 })
 
@@ -90,7 +89,7 @@ api.nvim_create_autocmd({ "BufEnter" }, {
 	group = au_group,
 	callback = function()
 		plugin_name = "todo-comments.nvim"
-		timer(plugin_name, 3000)
+		defer_fn(plugin_name, 3000)
 	end,
 })
 
@@ -98,7 +97,7 @@ api.nvim_create_autocmd({ "InsertEnter" }, {
 	group = au_group,
 	callback = function()
 		plugin_name = "nvim-autopairs"
-		timer(plugin_name, 3000)
+		defer_fn(plugin_name, 3000)
 	end,
 })
 
@@ -107,7 +106,7 @@ api.nvim_create_autocmd({ "InsertEnter" }, {
 	callback = function()
 		plugin_name = "markdown-preview.nvim"
 		if bo.filetype == "markdown" then
-			timer(plugin_name, 3000)
+			defer_fn(plugin_name, 3000)
 		end
 	end,
 })
