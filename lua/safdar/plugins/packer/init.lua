36cd63c401ac84eb90c73a8545a41b3151a66630
TheSafdarAwan <thesafdarawan@gmail.com>
2022-12-26 18:00:56 +0500

    update

--- lua/safdar/plugins/packer/init.lua
+++ lua/safdar/plugins/packer/init.lua
@@ -270,18 +270,9 @@ local plugins = {
 	----------------------------------------------------------------------
 	--                            ~> Lsp                                --
 	----------------------------------------------------------------------
-	["b0o/schemastore.nvim"] = {
-		opt = true,
-		setup = function()
-			local plugin = {
-				name = "schemastore.nvim",
-			}
-			require("lazy-loader").loaders.schedule_autocmd(plugin)
-		end,
-	}, -- for json schemas
+	["b0o/schemastore.nvim"] = { event = "BufWinEnter" }, -- for json schemas
 	["neovim/nvim-lspconfig"] = {
 		after = "schemastore.nvim",
-		module_pattern = { "lspconfig.*" },
 		config = function()
 			require("safdar.lsp.lsp")
 		end,
@@ -328,23 +319,9 @@ local plugins = {
 	--                ~> Completion and Snippets                        --
 	----------------------------------------------------------------------
 	["hrsh7th/nvim-cmp"] = {
-		opt = true,
-		event = "InsertEnter",
+		evnet = "InsertEnter",
 		setup = function()
-			local plugin = {
-				name = "undotree",
-				before_load = {
-					config = function()
-						require("fused").lazy_load("cmp")
-					end,
-				},
-				registers = {
-					autocmd = {
-						event = "InsertEnter",
-					},
-				},
-			}
-			require("lazy-loader").loader(plugin)
+			require("fused").lazy_load("cmp")
 		end,
 		config = function()
 			require("safdar.lsp.cmp")
--- lua/safdar/plugins/undotree/maps.lua
+++ lua/safdar/plugins/undotree/maps.lua
@@ -1,3 +1,4 @@
 local map = require("safdar.core.utils").set_map
 --> Undotree
+-- NOTE: now this is done inside the lazy-loader.nvim
 -- map("n", "<leader>u", ":UndotreeToggle<cr> :wincmd h<cr>", { noremap = true, silent = true })
