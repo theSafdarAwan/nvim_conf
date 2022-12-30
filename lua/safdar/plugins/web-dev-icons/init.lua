308067036e61a0186bf571a488c0003490844c8c
TheSafdarAwan <thesafdarawan@gmail.com>
2022-12-30 17:16:20 +0500

    update the dev-icons conf

--- lua/safdar/plugins/web-dev-icons/init.lua
+++ lua/safdar/plugins/web-dev-icons/init.lua
@@ -151,5 +151,5 @@ require("nvim-web-devicons").setup({
 	color_icons = true,
 	-- globally enable default icons (default to false)
 	-- will get overriden by `get_icons` option
-	default = true,
+	default = false,
 })
