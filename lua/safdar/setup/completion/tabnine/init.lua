b56d1a648626637b494c4922491a8c3c40e740de
TheSafdarAwan <thesafdarawan@gmail.com>
2023-01-01 09:39:33 +0500

    load tabnine after cmp

--- lua/safdar/setup/completion/tabnine/init.lua
+++ lua/safdar/setup/completion/tabnine/init.lua
@@ -2,16 +2,8 @@ local function plugin(install)
 	install({
 		"tzachar/cmp-tabnine",
 		opt = true,
+		after = "nvim-cmp",
 		run = "./install.sh",
-		setup = function()
-			local tabnine = {
-				name = "cmp-tabnine",
-				autocmd = {
-					events = "InsertEnter",
-				},
-			}
-			require("lazy-loader").load(tabnine)
-		end,
 		config = function()
 			require("safdar.setup.completion.tabnine.config").config()
 		end,
