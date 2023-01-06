40483fb6ef9d4d03ac42d2ac04c93a5f56916540
TheSafdarAwan <thesafdarawan@gmail.com>
2023-01-07 03:26:56 +0500

    Changes for in plan

--- lua/safdar/setup/writing/neorg/init.lua
+++ lua/safdar/setup/writing/neorg/init.lua
@@ -21,7 +21,7 @@ local function plugin(install)
 			require("lazy-loader").load(lazy_load)
 		end,
 		-- NOTE: at the moment cant do the commands when lazy loading
-		-- plugin with lazy-loader.nvim
+		-- plugin with lazy-loader.
 		-- run = ":Neorg sync-parsers", -- This is the important bit!
 		config = function()
 			require("safdar.setup.writing.neorg.config").config()
