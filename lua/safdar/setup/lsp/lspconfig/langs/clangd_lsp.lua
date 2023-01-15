ec4d8026b724e398755077ad1128400ee2239c04
TheSafdarAwan <thesafdarawan@gmail.com>
2023-01-15 20:08:58 +0500

    fix: fix the mistake of not adding on_attach for clangd_lsp

--- lua/safdar/setup/lsp/lspconfig/langs/clangd_lsp.lua
+++ lua/safdar/setup/lsp/lspconfig/langs/clangd_lsp.lua
@@ -1,3 +1,7 @@
 local utils = require("safdar.setup.lsp.utils")
 local capabilities = utils.capabilities
-require("lspconfig").clangd.setup({ capabilities = capabilities })
+local on_attach = utils.on_attach
+require("lspconfig").clangd.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
