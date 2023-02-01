35d67c1c41aad99e32d86c099ee350aee68a5e0a
TheSafdarAwan <thesafdarawan@gmail.com>
2023-02-01 05:33:50 +0500

    added the file-toggler for toggling between last two files

--- after/plugin/personal/file-toggler.lua
+++ after/plugin/personal/file-toggler.lua
@@ -2,36 +2,48 @@ local files_info = {}
 
 local api = vim.api
 
-api.nvim_create_autocmd("BufLeave", {
-	group = api.nvim_create_augroup("file toggler", { clear = true }),
+api.nvim_create_autocmd("BufWinLeave", {
+	group = api.nvim_create_augroup("file toggler BufLeave", { clear = true }),
 	callback = function()
-		-- check if the buffer has a name or is created
 		local buf_name = api.nvim_buf_get_name(0)
-		if #buf_name < 1 then
-			return
-		end
+		local file = {}
+		file.file_name = buf_name
+		file.buf_nr = vim.fn.bufnr()
+		file.cursor = api.nvim_win_get_cursor(0)
 
-		local is_hidden = api.nvim_buf_get_option(0, "bufhidden")
-		if #is_hidden > 1 then
-			return
-		end
+		-- need to check the next buffer before adding the previous one
+		api.nvim_create_autocmd("BufWinEnter", {
+			group = api.nvim_create_augroup("file toggler BufWinEnter", { clear = true }),
+			callback = function()
+				-- returns if the hidden string is not empty and the filetype isn't an
+				-- empty string
+				local buf_is_listed = api.nvim_buf_get_option(0, "buflisted")
+				if not buf_is_listed or #vim.bo.filetype < 1 then
+					return
+				end
 
-		-- need to make sure that the first and the second files are not the same
-		if #files_info > 1 and files_info[1].file_name == files_info[2].file_name then
-			return
-		end
+				-- need to make sure that the first and the second files are not the same
+				if file.file_name == api.nvim_buf_get_name(0) then
+					return
+				end
 
-		if #files_info > 1 then
-			files_info[2] = files_info[1]
-		end
+				-- check if the first key is present in the files_info table then move it
+				-- to the second
+				if files_info[1] then
+					files_info[2] = file
+				end
 
-		files_info[1] = {}
-		files_info[1].file_name = buf_name
-		files_info[1].cursor = api.nvim_win_get_cursor(0)
+				files_info[1] = file
+			end,
+		})
 	end,
 })
 
 vim.keymap.set("n", "<leader>ll", function()
+	if #files_info < 1 then
+		return
+	end
+
 	vim.cmd("edit " .. files_info[1].file_name)
 	vim.schedule_wrap(function()
 		api.nvim_win_set_cursor(0, files_info[1].cursor)
