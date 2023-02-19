60559f936291d9c7e6e83d9074ed00032071bd94
TheSafdarAwan <thesafdarawan@gmail.com>
2023-02-19 19:56:14 +0500

    cfg(nvim-treesitter-textobjects): added maps for the text-objects

--- lua/safdar/setup/ui/treesitter/text-objects/config.lua
+++ lua/safdar/setup/ui/treesitter/text-objects/config.lua
@@ -2,12 +2,43 @@ local function config()
 	require("nvim-treesitter.configs").setup({
 		textobjects = {
 			swap = {
-				enable = true,
 				swap_next = {
-					["gsn"] = "@parameter.inner",
+					["gsn"] = {
+						query = "@parameter.inner",
+						desc = "Swap current parameter with next parameter",
+					},
 				},
 				swap_previous = {
-					["gsp"] = "@parameter.inner",
+					["gsp"] = {
+						query = "@parameter.inner",
+						desc = "Swap current parameter with previous parameter",
+					},
+				},
+			},
+			move = {
+				set_jumps = true,
+				goto_next_start = {
+					["gnf"] = { query = "@function.outer", desc = "Next Function start" },
+					["gnc"] = { query = "@class.outer", desc = "Next class start" },
+					["gns"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+					["gni"] = { query = "@conditional.outer", desc = "Next Conditional start" },
+					["gnl"] = { query = "@loop.outer", desc = "Next loop start" },
+				},
+				goto_next_end = {
+					["gnF"] = { query = "@function.outer", desc = "Next Function end" },
+					["gnC"] = { query = "@class.outer", desc = "Next Class end" },
+					["gnI"] = { query = "@conditional.outer", desc = "Next Conditional end" },
+					["gnL"] = { query = "@loop.outer", desc = "Next loop end" },
+				},
+				goto_previous_start = {
+					["gpf"] = { query = "@function.outer", desc = "Previous Function start" },
+					["gpc"] = { query = "@class.outer", desc = "Previous Class start" },
+					["gpi"] = { query = "@conditional.outer", desc = "Previous Conditional start" },
+				},
+				goto_previous_end = {
+					["gpF"] = { query = "@function.outer", desc = "Previous Function end" },
+					["gpC"] = { query = "@class.outer", desc = "Previous Class end" },
+					["gpI"] = { query = "@conditional.outer", desc = "Previous conditional end" },
 				},
 			},
 		},
