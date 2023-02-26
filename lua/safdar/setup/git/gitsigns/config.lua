be5e22d205a6c377c157533b3a27ba0529b00653
TheSafdarAwan <thesafdarawan@gmail.com>
2023-02-26 20:27:42 +0500

    refactor(gitsigns): move gitsigns setup function to init

--- lua/safdar/setup/git/gitsigns/config.lua
+++ lua/safdar/setup/git/gitsigns/config.lua
@@ -53,30 +53,4 @@ local config = function()
 	})
 end
 
-local callback = function()
-	-- this variable is just for me
-	vim.g.__git_is_ok = false
-	vim.fn.system("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse")
-	if vim.v.shell_error == 0 then
-		vim.g.__git_is_ok = true
-		return true
-	end
-end
-
-local setup = function()
-	local gitsigns = {
-		name = "gitsigns.nvim",
-		on_load = {
-			config = function()
-				require("safdar.setup.git.gitsigns.config").config()
-			end,
-		},
-		autocmd = {
-			event = "BufRead",
-			callback = callback,
-		},
-	}
-	require("lazy-loader").load(gitsigns)
-end
-
-return { config = config, setup = setup }
+return { config = config }
--- lua/safdar/setup/git/gitsigns/init.lua
+++ lua/safdar/setup/git/gitsigns/init.lua
@@ -3,7 +3,28 @@ local function plugin(install)
 		opt = true,
 		"lewis6991/gitsigns.nvim",
 		setup = function()
-			require("safdar.setup.git.gitsigns.config").setup()
+			local gitsigns = {
+				name = "gitsigns.nvim",
+				on_load = {
+					config = function()
+						require("safdar.setup.git.gitsigns.config").config()
+					end,
+				},
+				autocmd = {
+					event = "BufRead",
+					callback = function()
+						-- this variable is just for me
+						vim.g.__git_is_ok = false
+						vim.fn.system("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse")
+						if vim.v.shell_error == 0 then
+							vim.g.__git_is_ok = true
+							return true
+						end
+					end
+,
+				},
+			}
+			require("lazy-loader").load(gitsigns)
 		end,
 	})
 end
