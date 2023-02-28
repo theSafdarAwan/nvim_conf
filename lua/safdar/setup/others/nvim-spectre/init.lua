d233b28f18787f0b719b1f710f5a8b7de088af0e
TheSafdarAwan <thesafdarawan@gmail.com>
2023-02-28 13:00:41 +0500

    cfg(nvim-spectre): lazy load nvim-spectre

--- lua/safdar/setup/others/nvim-spectre/init.lua
+++ lua/safdar/setup/others/nvim-spectre/init.lua
@@ -1,6 +1,8 @@
 local function plugin(install)
 	install({
+		opt = true,
 		"windwp/nvim-spectre",
+		keys = { "=" },
 		config = function()
 			require("safdar.setup.others.nvim-spectre.config").config()
 			require("safdar.setup.others.nvim-spectre.maps").maps()
