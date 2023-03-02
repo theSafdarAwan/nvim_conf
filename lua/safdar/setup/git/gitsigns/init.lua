f345aa6e31e0cef1d88101836840e7a6ea965c64
TheSafdarAwan <thesafdarawan@gmail.com>
2023-03-02 13:57:17 +0500

    cfg(added comments): added comments to the gitsigns callback function for lazy-loader

--- lua/safdar/setup/git/gitsigns/init.lua
+++ lua/safdar/setup/git/gitsigns/init.lua
@@ -13,15 +13,17 @@ local function plugin(install)
 				autocmd = {
 					event = "BufRead",
 					callback = function()
-						-- this variable is just for me
+						-- this variable is just for my status line which changes the git
+						-- git status
 						vim.g.__git_is_ok = false
+						-- if this commands returns true the next guard will become true and
+						-- the plugin will be loaded
 						vim.fn.system("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse")
 						if vim.v.shell_error == 0 then
 							vim.g.__git_is_ok = true
 							return true
 						end
-					end
-,
+					end,
 				},
 			}
 			require("lazy-loader").load(gitsigns)
