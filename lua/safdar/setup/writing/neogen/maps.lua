6849524ab12ab470ed6f06e4a22827d10b13a525
TheSafdarAwan <thesafdarawan@gmail.com>
2023-03-14 03:29:04 +0500

    fix: fixed neogen maps for insert mode

--- lua/safdar/setup/writing/neogen/maps.lua
+++ lua/safdar/setup/writing/neogen/maps.lua
@@ -5,5 +5,5 @@ set_map("n", "[nc", ":lua require('neogen').generate({ type = 'class' })<CR>")
 set_map("n", "[nt", ":lua require('neogen').generate({ type = 'type' })<CR>")
 
 -- snippet's movements
-set_map("i", "<C-j>", ":lua require('neogen').jump_next<CR>")
-set_map("i", "<C-k>", ":lua require('neogen').jump_prev<CR>")
+set_map("i", "<C-j>", "<cmd>lua require('neogen').jump_next<CR>")
+set_map("i", "<C-k>", "<cmd>lua require('neogen').jump_prev<CR>")
