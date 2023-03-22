36cd7c3282c4f3be37d3bb86713965fbc06452fa
TheSafdarAwan <thesafdarawan@gmail.com>
2023-03-22 19:32:05 +0500

    fix: removed the event from cmp-dictionary lazy.nvim spec

--- lua/safdar/setup/completion/cmp-dictionary/init.lua
+++ lua/safdar/setup/completion/cmp-dictionary/init.lua
@@ -2,7 +2,6 @@ local function plugin(install)
 	install({
 		"uga-rosa/cmp-dictionary",
 		ft = { "md", "html", "norg" },
-		event = "CursorMoved",
 		config = function()
 			require("safdar.setup.completion.cmp-dictionary.config").config()
 		end,
