c77c9febaf5b86ed6b07aae6fe7902dbf3ea9ff3
TheSafdarAwan <thesafdarawan@gmail.com>
2023-03-30 21:55:28 +0500

    cfg: added some capture queries for javascript

--- after/queries/javascript/highlights.scm
+++ after/queries/javascript/highlights.scm
@@ -1,4 +1,16 @@
 ;; extends
-; ((call_expression)
-;     function: (member_expression)
-;       object: (identifier) @js.console)
+
+; capture the functions names, called which are members of an object.
+; (call_expression
+;     function: (member_expression
+;       object: (identifier) @capture))
+
+; capture the variable with binary expression
+; ((variable_declarator
+; 	name: (identifier)
+; 	value: (binary_expression)) @capture)
+
+; capture the arrow function in an object
+; ((pair
+; 	key: (property_identifier)
+; 	value: (arrow_function)) @capture)
