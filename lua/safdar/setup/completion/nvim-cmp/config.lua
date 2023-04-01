adc30dc4666be411ed8e5b89da8a49eb242af613
TheSafdarAwan <thesafdarawan@gmail.com>
2023-04-01 23:43:23 +0500

    cfg: added a custom cmp formatting function

--- lua/safdar/setup/completion/nvim-cmp/config.lua
+++ lua/safdar/setup/completion/nvim-cmp/config.lua
@@ -1,3 +1,22 @@
+-- TODO: ??????????
+local function formatForTailwindCSS(entry, vim_item)
+	if vim_item.kind == "Color" and entry.completion_item.documentation then
+		local _, _, r, g, b = string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
+		if r then
+			local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
+			local group = "Tw_" .. color
+			if vim.fn.hlID(group) < 1 then
+				vim.api.nvim_set_hl(0, group, { fg = "#" .. color })
+			end
+			vim_item.kind = "●"
+			vim_item.kind_hl_group = group
+			return vim_item
+		end
+	end
+	vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+	return vim_item
+end
+
 local config = function()
 	-- luasnip setup
 	local border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
