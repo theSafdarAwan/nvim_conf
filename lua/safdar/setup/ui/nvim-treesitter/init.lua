01b0f31c27a20724c36eff2219a517640100d1a0
TheSafdarAwan <thesafdarawan@gmail.com>
2023-04-02 08:47:04 +0500

    feat: added iswap.nvim

--- lua/safdar/setup/ui/nvim-treesitter/init.lua
+++ lua/safdar/setup/ui/nvim-treesitter/init.lua
@@ -60,6 +60,22 @@ local function plugin(install)
 			})
 		end,
 	})
+	install({
+		"mizlan/iswap.nvim",
+		keys = "gsi",
+		config = function()
+			require("iswap").setup({
+				-- Automatically swap with only two arguments
+				-- default nil
+				autoswap = true,
+				-- Highlight group for the greyed background
+				-- default 'Comment'
+				hl_grey = "",
+
+			})
+			vim.keymap.set("n", "gsi", ":ISwapWith<CR>", { silent = true, noremap = true })
+		end,
+	})
 
 	require("safdar.setup.ui.nvim-treesitter.text-objects").install(install)
 end
