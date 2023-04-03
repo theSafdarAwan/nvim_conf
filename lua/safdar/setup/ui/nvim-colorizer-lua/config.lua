c66fc0b5f31795e69ddbe7f18788b59d92784f64
TheSafdarAwan <thesafdarawan@gmail.com>
2023-04-03 22:08:52 +0500

    cfg: changes to the cfg of colorizer

--- lua/safdar/setup/ui/nvim-colorizer-lua/config.lua
+++ lua/safdar/setup/ui/nvim-colorizer-lua/config.lua
@@ -1,10 +1,6 @@
 local function config()
-	-- Use the `default_options` as the second parameter, which uses
-	-- `foreground` for every mode. This is the inverse of the previous
-	-- setup configuration.
-	require("colorizer").setup({
+	local color_settings = {
 		filetypes = {
-			"*", -- Highlight all files, but customize some others.
 			css = {
 				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
 				-- RGB = true, -- #000 hex codes
@@ -14,22 +10,26 @@ local function config()
 				-- rgb_fn = true, -- CSS rgb(1,3,101) and rgba() functions
 				-- hsl_fn = true, -- CSS hsl() and hsla() functions
 				-- css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-				names = true, -- "Name" codes like Blue or blue
+				-- names = true, -- "Name" codes like Blue or blue
 			},
 			cmp_docs = {
 				always_update = true,
-				names = true,
+				css = true,
 			},
-			"!prompt",
-			"!popup",
+			lua = {
+				css = true,
+				names = false,
+			}
 		},
 		user_default_options = {
-			names = false,
-			-- Available methods are false / true / "normal" / "lsp" / "both"
-			-- True is same as normal
-			tailwind = true, -- Enable tailwind colors
+			css = false,
+			-- tailwind = true, -- Enable tailwind colors
 		},
-	})
+	}
+	-- Use the `default_options` as the second parameter, which uses
+	-- `foreground` for every mode. This is the inverse of the previous
+	-- setup configuration.
+	require("colorizer").setup(color_settings)
 end
 
 return { config = config }
