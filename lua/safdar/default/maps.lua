ec11a78b003740a95d471e12da460fd5900bab5e
TheSafdarAwan <thesafdarawan@gmail.com>
2023-04-21 22:27:34 +0500

    map: added delete current file map `<leader>dd`

--- lua/safdar/default/maps.lua
+++ lua/safdar/default/maps.lua
@@ -40,6 +40,34 @@ end)
 set_map("n", "Y", "y$")
 set_map("v", "Y", "m`$y``")
 
+-- WARN: this map deletes the current file from the file system, it asks the user
+-- to for input using `vim.ui.select`
+-- but it won't delete the buffer so if you accidentally deleted a file you can
+-- recover the buffer by doing :w
+set_map("n", "<leader>dd", function()
+	local file_location = vim.fn.expand("%:p")
+	local file_name = vim.fn.expand("%:t")
+	local ui_select_menu = {
+		"yes",
+		"no",
+	}
+	local ui_select_opts = {
+		prompt = "Do you want to delete " .. file_name .. " ?",
+		format_item = function(item)
+			local title = item
+			return title
+		end,
+	}
+	vim.ui.select(ui_select_menu, ui_select_opts, function(choice)
+		if choice == "yes" then
+			vim.fn.system({ "rm", "-rf", file_location })
+			print("file deleted!")
+		else
+			print("file deletion canceled")
+		end
+	end)
+end)
+
 -- Best remap ever
 set_map("n", "cn", "*``cgn")
 set_map("n", "cN", "*``cgN")
@@ -77,7 +105,7 @@ set_map("n", "<A-h>", "zh")
 -- set_map("n", "<C-m>", ":messages<CR>") -- i am using noice.nvim
 
 -- Paste From the system clipboard
-set_map("n", "<leader>p", '"+p')
+set_map("n", "<leader>p", "\"+p")
 -- Paste onto the selection and don't copy the selection to the reg
 set_map("v", "<leader>p", "_dP")
 
@@ -89,14 +117,14 @@ set_map("n", "<ESC>", ":noh<cr><c-l>")
 
 -- use backspace to change the selection
 -- only for the @luasnip selection
-set_map("v", "<BS>", '"_c')
+set_map("v", "<BS>", "\"_c")
 
 -- Indentation
 set_map("v", "<", "<gv")
 set_map("v", ">", ">gv")
 
 -- delete into the void in visual mode
-set_map("v", "D", '"_d')
+set_map("v", "D", "\"_d")
 
 -- -- Resize the buffer window
 -- set_map("n", "<leader>=", ":vertical resize +5<cr>")
